a83bb661969516c7a427ff3da8e246a2954d6a07
yeyetha <128303710+yeyetha@users.noreply.github.com>
2024-12-28 14:44:24 +0700

    Delete yt_inventory_management directory

--- yt_inventory_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import inventory_item
-from . import material_request
-from . import stock_transfer
-from . import budget_control
-from . import product_template
--- yt_inventory_management/models/product_template.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplateInherit(models.Model):
-    _inherit = 'product.template'
-
-    
-    additional_field = fields.Char(string="Additional Info")
